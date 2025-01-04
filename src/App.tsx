0b2931f57c16b7655c51440c329e91a6ad91661f
rodriguezmauricio <contato.mauricioalves@gmail.com>
2025-01-04 19:12:15 +0000

    base card structure test

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import "./App.css";
+import Card from "./components/card/Card";
 
 function App() {
     return (
         <main className="game">
             <h1>GAME</h1>
             <div className="opponent"></div>
-            <div className="player"></div>
+            <div className="player">
+                <Card />
+            </div>
         </main>
     );
 }
---
+++ src/components/card/Card.tsx
@@ -0,0 +1,11 @@
+const Card = () => {
+    return (
+        <div>
+            <h2>Title</h2>
+            <h3>atk:</h3>
+            <h3>dfs:</h3>
+        </div>
+    );
+};
+
+export default Card;
