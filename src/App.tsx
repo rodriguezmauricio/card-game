19209cbaa35f85b87f6cee8800733fc8a78f0b33
rodriguezmauricio <contato.mauricioalves@gmail.com>
2025-01-04 19:09:35 +0000

    base structure

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+    return (
+        <main className="game">
+            <h1>GAME</h1>
+            <div className="opponent"></div>
+            <div className="player"></div>
+        </main>
+    );
+}
+
+export default App;
