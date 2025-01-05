ef42524a6cc52e868c151cf7e42aae9748e2dd61
rodriguezmauricio <contato.mauricioalves@gmail.com>
2025-01-05 21:13:31 +0000

    added card base style

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,6 +1,8 @@
+import styles from "./Card.module.css";
+
 const Card = () => {
     return (
-        <div>
+        <div className={styles.container}>
             <h2>Title</h2>
             <h3>atk:</h3>
             <h3>dfs:</h3>
