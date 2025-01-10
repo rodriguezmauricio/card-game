3be6d673adf1fea5ba5775460c58f0cd52c442eb
rodriguezmauricio <contato.mauricioalves@gmail.com>
2025-01-10 20:30:08 +0000

    added cards file

---
+++ src/files/cards.ts
@@ -0,0 +1,20 @@
+export const cards = [
+    {
+        id: 1,
+        name: "monster 1",
+        atk: 5,
+        def: 3,
+    },
+    {
+        id: 2,
+        name: "monster 2",
+        atk: 3,
+        def: 3,
+    },
+    {
+        id: 2,
+        name: "monster 3",
+        atk: 2,
+        def: 5,
+    },
+];
